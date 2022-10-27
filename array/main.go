403569575fa99549c58f840ef50f3e13dff6f283
mehedicse133 <mehedicse047@gmail.com>
2022-10-27 16:41:11 +0600

    array initial commit

---
+++ array/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("This is arry module")
+
+	var animals [3]string
+
+	animals[0] = "Dog"
+	animals[1] = "Cat"
+	animals[2] = "Tiger"
+
+	fmt.Println("The animals list are : ", animals)
+}
