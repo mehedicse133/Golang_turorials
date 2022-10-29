939d7b591ac0e4effed50ecf8d656c11116918ce
mehedicse133 <mehedicse047@gmail.com>
2022-10-29 13:53:05 +0600

    extend else_if func

--- conditions/main.go
+++ conditions/main.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"fmt"
+	"strings"
 )
 
 // main function
 func main() {
+
+	else_if_conition("My namem is Mehedi")
 
 	// fmt.Println("Enter your age: ")
 	// var age int
@@ -15,30 +18,30 @@ func main() {
 	// // Pass age in variable into age_check func as argument
 	// age_check(age)
 
-	var student []string
-	// var firstname, lastname string
+	// var student []string
+	// // var firstname, lastname string
 
-	fmt.Println("Enter how many student you take input:")
-	var num_student int
-	fmt.Scan(&num_student)
+	// fmt.Println("Enter how many student you take input:")
+	// var num_student int
+	// fmt.Scan(&num_student)
 
-	for i := 0; i < num_student; i++ {
-		var firstname, lastname string
+	// for i := 0; i < num_student; i++ {
+	// 	var firstname, lastname string
 
-		fmt.Println("Enter first name : ")
-		fmt.Scan(&firstname)
-		fmt.Println(firstname)
+	// 	fmt.Println("Enter first name : ")
+	// 	fmt.Scan(&firstname)
+	// 	fmt.Println(firstname)
 
-		fmt.Println("Enter last name : ")
-		fmt.Scan(&firstname)
-		full_name := firstname + " " + lastname
-		fmt.Println(full_name)
-		student = append(student, full_name)
-	}
+	// 	fmt.Println("Enter last name : ")
+	// 	fmt.Scan(&firstname)
+	// 	full_name := firstname + " " + lastname
+	// 	fmt.Println(full_name)
+	// 	student = append(student, full_name)
+	// }
 
-	fmt.Println(student)
-	fmt.Println(len(student))
-	fmt.Println(cap(student))
+	// fmt.Println(student)
+	// fmt.Println(len(student))
+	// fmt.Println(cap(student))
 
 	// fmt.Println("Enter first name : ")
 	// fmt.Scan(&firstname)
@@ -67,4 +70,27 @@ func age_check(age int) {
 	} else {
 		fmt.Println("Yor are teen age")
 	}
+}
+
+func else_if_conition(str string) {
+	fmt.Println(len(str))
+
+	s := strings.SplitAfter(str, "")
+
+	var m_volume []string
+
+	count := 0
+	for i := 0; i < len(s); i++ {
+		if s[i] == " " {
+			continue
+		}
+		count += 1
+
+		if s[i] == "m" {
+			m_volume = append(m_volume, s[i])
+		}
+
+	}
+	fmt.Println(count)
+	fmt.Println(len(m_volume))
 }
