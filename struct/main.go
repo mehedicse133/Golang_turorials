d0cd288da4e931191073db0cfd69f54b3f83f2ac
mehedicse133 <mehedicse047@gmail.com>
2022-10-31 12:12:44 +0600

    add comment

--- struct/main.go
+++ struct/main.go
@@ -13,8 +13,8 @@ type student struct {
 
 func details_student(s student) {
 	fmt.Println("Details of student:", s.name)
-	fmt.Printf("id = %v\nname = %v\nage = %v\ncity =  %v\naddress = %v\nemail = %v", s.id, s.name, s.age, s.city, s.address, s.email)
-
+	fmt.Printf("id = %v\nname = %v\nage = %v\ncity =  %v\naddress = %v\nemail = %v\n", s.id, s.name, s.age, s.city, s.address, s.email)
+	fmt.Println("--------------------------------")
 }
 
 type student_list struct {
@@ -27,24 +27,30 @@ func (slist *student_list) add_student(s student) {
 
 func main() {
 
-	//var student_list []string
+	//created object of student struct
 	s1 := student{22, "mehedi", 30, "Feni", "Dhaka", "m@gmail.com"}
 	s2 := student{2, "Hossain", 31, "Feni", "Dhaka", "h@gmail.com"}
 
-	s := student_list{}
+	//print student objects
+	fmt.Println(s1.name)
+	fmt.Println("--------------------------------")
+	fmt.Println(s2.name)
+	fmt.Println("--------------------------------")
 
+	// print both students objents
+	fmt.Println(s1, s2)
+	fmt.Println("--------------------------------")
+
+	//print single student object using function
+	details_student(s1)
+	details_student(s2)
+
+	// created object of student_list struct
+	s := student_list{}
 	s.add_student(s1)
 	s.add_student(s2)
 
-	fmt.Println(len(s.students_list))
 	fmt.Println(s.students_list)
+	fmt.Println("Length of slice students list : ", len(s.students_list))
 
-	fmt.Println(s1.name)
-	fmt.Println(s2.name)
-	fmt.Println(s1, s2)
-	fmt.Println()
-
-	details_student(s1)
-
-	details_student(s2)
 }
