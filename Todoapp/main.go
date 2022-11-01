5775e8023b65665c8f7791e678e4e0fcacafb78f
mehedicse133 <mehedicse047@gmail.com>
2022-11-01 18:46:46 +0600

    initial commit

---
+++ Todoapp/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"text/template"
+)
+
+var tmpl *template.Template
+
+type Todo struct {
+	Item string
+	Done bool
+}
+
+type PageData struct {
+	Title string
+	Todos []Todo
+}
+
+func todo(w http.ResponseWriter, r *http.Request) {
+	tmpl = template.Must(template.ParseFiles("templates/home.html"))
+	data := PageData{
+		Title: "Todo list",
+		Todos: []Todo{
+			{Item: "a", Done: true},
+			{Item: "b", Done: false},
+			{Item: "c", Done: false},
+		},
+	}
+
+	tmpl.Execute(w, data)
+}
+
+func main() {
+	fmt.Printf("Starting server at port 8088\n")
+	mux := http.NewServeMux()
+	mux.HandleFunc("/todo", todo)
+	log.Fatal(http.ListenAndServe(":8099", mux))
+
+}
