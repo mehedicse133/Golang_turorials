aa75ce5f4abae7f1f00cf4b64c521befe6c6b6d1
mehedicse133 <mehedicse047@gmail.com>
2022-11-02 18:42:13 +0600

    add comment

--- interface/main.go
+++ interface/main.go
@@ -2,21 +2,24 @@ package main
 
 import "fmt"
 
-type stduent struct {
-	name string
-	age  int
-}
-
+// interface contains colletions of functions
 type persondetails interface {
 	getname() string
 	getage() int
 }
 
+// struct contains collections of fields
 type teacher struct {
 	name string
 	age  int
 }
 
+type stduent struct {
+	name string
+	age  int
+}
+
+// create teacher and student functions
 func (s stduent) getname() string {
 	return s.name
 }
@@ -42,9 +45,11 @@ func pirnt(p persondetails) {
 func main() {
 	fmt.Println("welcome to interface")
 
+	// create object of student and teacher
 	stu := stduent{name: "mehedi", age: 30}
 	tea := teacher{name: "amam", age: 30}
 
+	// pass objects into interface method
 	pirnt(stu)
 	pirnt(tea)
 
