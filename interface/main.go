b6b5f1a4d97ef008ae750565db1b621c4bb17a96
mehedicse133 <mehedicse047@gmail.com>
2022-11-02 18:38:20 +0600

    add interface functionalities

--- interface/main.go
+++ interface/main.go
@@ -2,7 +2,50 @@ package main
 
 import "fmt"
 
+type stduent struct {
+	name string
+	age  int
+}
+
+type persondetails interface {
+	getname() string
+	getage() int
+}
+
+type teacher struct {
+	name string
+	age  int
+}
+
+func (s stduent) getname() string {
+	return s.name
+}
+
+func (s stduent) getage() int {
+	return s.age
+}
+
+func (t teacher) getname() string {
+	return t.name
+}
+
+func (t teacher) getage() int {
+	return t.age
+}
+
+func pirnt(p persondetails) {
+	fmt.Println(p.getname())
+	fmt.Println(p.getage())
+
+}
+
 func main() {
 	fmt.Println("welcome to interface")
+
+	stu := stduent{name: "mehedi", age: 30}
+	tea := teacher{name: "amam", age: 30}
+
+	pirnt(stu)
+	pirnt(tea)
 
 }
