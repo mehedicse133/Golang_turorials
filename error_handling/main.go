b6ec2a0b754f162f0d6829841e815ded932048df
mehedicse133 <mehedicse047@gmail.com>
2022-11-02 11:40:24 +0600

    second commit

--- error_handling/main.go
+++ error_handling/main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 func main() {
 	fmt.Println("Welcome to error handling")
+
+	result, err := div(7, 2)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(result)
+
+}
+
+func div(a int, b int) (int, error) {
+	if b == 0 {
+		return -1, errors.New("connot divide by 0")
+	}
+	return a / b, nil
+
 }
