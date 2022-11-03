a991e0bf7fad5941b76346353a19dbd0df56d3ce
mehedicse133 <mehedicse047@gmail.com>
2022-11-03 12:23:19 +0600

    name_extract_form_emails

--- strings/main.go
+++ strings/main.go
@@ -16,10 +16,7 @@ func main() {
 	fmt.Println("String is : ", str)
 
 	// using string Split Method
-	split_str := strings.Split(str, "")
-	for i := 0; i < len(split_str); i++ {
-		fmt.Println("Split string char is : ", split_str[i])
-	}
+	split_method()
 
 	// using string Replace method
 	str2 := "Mehedi Hosain"
@@ -46,16 +43,7 @@ func main() {
 	print_line()
 
 	// Name collect form list of email using string split() method
-	emaillist := []string{"me@gmail.com", "ta@gmail.com", "am@gmail.com", "su@gmail.com"}
-	var name_list []string
-	for _, email := range emaillist {
-		split_name := strings.Split(email, "@")
-		name := split_name[0]
-		name_list = append(name_list, name)
-
-	}
-	fmt.Println("Output os Extract name list form email:", name_list) //Output is [me ta am su]
-	print_line()
+	name_extract_form_emails()
 
 	// using Field() method split the string which work on based os white space
 	field_method()
@@ -73,6 +61,14 @@ func count_method() {
 	count := strings.Count(str, "Lorem")
 	fmt.Println(count)
 	print_line()
+}
+
+func split_method() {
+	var str = "Mehedi"
+	split_str := strings.Split(str, "")
+	for i := 0; i < len(split_str); i++ {
+		fmt.Println("Split string char is : ", split_str[i])
+	}
 }
 
 func has_prefix_method() {
@@ -103,6 +99,19 @@ func field_method() {
 		fmt.Println(l)
 	}
 	fmt.Println(len(l_string))
+	print_line()
+}
+
+func name_extract_form_emails() {
+	emaillist := []string{"me@gmail.com", "ta@gmail.com", "am@gmail.com", "su@gmail.com"}
+	var name_list []string
+	for _, email := range emaillist {
+		split_name := strings.Split(email, "@")
+		name := split_name[0]
+		name_list = append(name_list, name)
+
+	}
+	fmt.Println("Output os Extract name list form email:", name_list) //Output is [me ta am su]
 	print_line()
 }
 
