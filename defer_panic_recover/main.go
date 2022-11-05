bd91e5edc9f105120f445c9ca7a077cfdb7d640f
mehedicse133 <mehedicse047@gmail.com>
2022-11-05 13:07:40 +0600

    add panicc_method

--- defer_panic_recover/main.go
+++ defer_panic_recover/main.go
@@ -12,4 +12,22 @@ func main() {
 	defer fmt.Println("B")
 	defer fmt.Println("C")
 
+	j := panicc("amam0", "tanvir", "subbir")
+	fmt.Println(j)
+
+	u := panicc("anik", "mamun", "rabbi")
+	fmt.Println(u)
+}
+
+func panicc(names ...string) []string {
+	var team []string
+	for _, name := range names {
+		if name == "mehedi" {
+			panic("The team is rejected,becuse there is a wrong person")
+		} else {
+			team = append(team, name)
+		}
+
+	}
+	return team
 }
